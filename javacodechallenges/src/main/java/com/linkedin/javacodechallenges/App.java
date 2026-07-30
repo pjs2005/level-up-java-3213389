c1a2a8a850a195dfaf27d68ba37cffbc22ff2ae4
pjs2005 <pjs2005@users.noreply.github.com>
2026-07-30 16:12:42 +0000

    changes

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,10 +3,34 @@ package com.linkedin.javacodechallenges;
 import java.util.Scanner;
 
 public class App {
+    private static final double CCF = 748;
+    private static final double MINI_CHARGE = 18.84;
+    private static final double MINI_CCF = 2;
+    private static final double ADD_CCF_FEE = 3.90;
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+
+        if (checkMinium(gallonsUsage)) {
+            return MINI_CHARGE;
+        } else {
+
+            double gallons_above = gallonsAbove(gallonsUsage);
+
+            double charableUnits = gallons_above / CCF;
+            charableUnits = Math.ceil(charableUnits);
+
+            double additinal = charableUnits * ADD_CCF_FEE;
+            return additinal + MINI_CHARGE;
+        }
+    }
+
+    private static double gallonsAbove(double gallonsUsage) {
+        double minimumgallon = CCF * MINI_CCF;
+        return gallonsUsage - minimumgallon;
+    }
+
+    private static boolean checkMinium(double gallonsUsage) {
+        return gallonsAbove(gallonsUsage) <= 0;
     }
 
     public static void main(String[] args) {
