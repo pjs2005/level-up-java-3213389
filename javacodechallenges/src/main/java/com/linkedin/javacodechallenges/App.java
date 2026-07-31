d1866401b19bdb2d4d491748803c1438e58e40b5
pjs2005 <pjs2005@users.noreply.github.com>
2026-07-31 15:19:19 +0000

    working

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,24 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    // Create function to calculate the date that's 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class App {
+    // Create function to calculate the date that's
     // 100 days from now
 
-    public static void main( String[] args )
-    {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 100);
+        LocalDateTime localDate = LocalDateTime.now().plusDays(100);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E dd MMM yyyy");
+        String dispayString = localDate.format(formatter);
+
+        System.out.println("100 days from now is... " + cal.getTime());
+        System.out.println("100 days from now is... " + dispayString);
+
     }
 }
