870239aafbc252a2c47910eb3ab5a6ef2392209c
pjs2005 <pjs2005@users.noreply.github.com>
2026-07-31 17:00:54 +0000

    working

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
@@ -17,11 +17,31 @@ public class StoreItem {
 
   public static Optional<StoreItem> findLeastExpensive(Collection<StoreItem> items) {
     // TODO: Implement
-    return Optional.empty();
+    Collection<StoreItem> calculatedItems = items
+        .stream().map(item -> {
+          String name = item.getName();
+          Double newPrice = item.getRetailPrice() * (1 - item.getDiscount());
+
+          return new StoreItem(name, newPrice, 0);
+        }).toList();
+    calculatedItems.stream().forEach(item -> System.out.println(item.display()));
+    return calculatedItems.stream().sorted((o1, o2) -> {
+      if (o1.getRetailPrice() < o2.getRetailPrice()) {
+        return -1;
+      } else if (o1.getRetailPrice() > o2.getRetailPrice()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }).findFirst();
   }
 
   @Override
   public String toString() {
     return "Name: " + name + ", " + "Retail price: " + retailPrice + ", " + "Discount " + discount;
+  }
+
+  public String display() {
+    return "Name: " + name + ", " + "Retail price: " + retailPrice;
   }
 }
